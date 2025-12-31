445a9e6a1d3d64a3355357d288bedfac79930e8d
saraUsama9910 <sarausama80@gmail.com>
2025-12-31 05:14:04 +0200

    logo

--- coders-cafe/src/components/navbar/Navbar.jsx
+++ coders-cafe/src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-
+import{ motion } from 'framer-motion'
 const Navbar = () => {
   return (
     <nav className='absolute top-0 left-0 w-full pt-10
@@ -12,8 +12,21 @@ const Navbar = () => {
    
     {/*logo section*/}
 
-    <h1 className='text-2xl font-semibold uppercase'> 
-        <span className='text-orange-300/90 font-bold pl-10'>Coders </span>Coffee</h1>
+    <motion.h1
+    initial={{ opacity: 0, y: -100 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+        delay: .2,
+    }}
+
+    
+    className='text-2xl font-semibold uppercase'> 
+        <span className='text-orange-300/90 font-bold pl-10'>
+        Coders </span>Coffee
+        </motion.h1>
 
 {/*hamburger section*/}
 <div>
