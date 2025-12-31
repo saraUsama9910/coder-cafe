2ed07cb6dc0a69e8ec6722a93e7fb195154fae70
saraUsama9910 <sarausama80@gmail.com>
2025-12-31 23:14:54 +0200

    s

--- coders-cafe/src/components/services/Services.jsx
+++ coders-cafe/src/components/services/Services.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="container my-16
+    space-y-4">
+        {/* header section  */}
+<div className='text-center max-w-lg mx-auto
+space-y-2'>
+<h1 className='text-3xl font-bold text-Gray-600/200'>
+    Fresh and <span className='text-yellow-500'>
+       Tasty Coffees
+    </span>
+</h1>
+
+</div>
+
+
+
+          {/* card section  */}
+
+
+
+    </div>
   )
 }
 
