1ac05dcb9bc8e8502eb8078ec1f22b2e90eaae1e
saraUsama9910 <sarausama80@gmail.com>
2025-12-31 23:29:40 +0200

    set animation

--- coders-cafe/src/components/services/Services.jsx
+++ coders-cafe/src/components/services/Services.jsx
@@ -8,13 +8,31 @@ const Services = () => {
 <div className='text-center max-w-lg mx-auto
 space-y-2'>
 <motion.h1 
-
+initial={{opacity:0,y:100}}
+whileInView={{opacity:1,y:0}}
+transition={{
+    type:"spring",
+    stiffness:150,
+    damping:10,
+    delay:0.2
+}}
 className='text-3xl font-bold text-Gray-600/200'>
     Fresh and <span className='text-yellow-500'>
        Tasty Coffees
     </span>
 </motion.h1>
-<motion.p className='text-sm opacity-50'>
+<motion.p 
+
+initial={{ opacity: 0, y: 100 }}
+whileInView={{ opacity: 1, y: 0 }}
+transition={{
+    type: "spring",
+    stiffness: 150,
+    damping: 10,
+    delay: 0.2
+}}
+
+className='text-sm opacity-50'>
 Lorem,ipsum dolor sit amet
 consectetur adipisicing elit.
 Eos 
