db6f4e2cf62823411c9123834b8316743ce88a42
saraUsama9910 <sarausama80@gmail.com>
2025-12-31 21:00:56 +0200

    creating services

--- coders-cafe/src/App.jsx
+++ coders-cafe/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
 const App = () => {
   return <div >
     <nav className="overflow-x-hidden">
       <Hero/>
-
+<Services/>
     </nav>
   </div>;
 };
