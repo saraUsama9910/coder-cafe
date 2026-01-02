7f56d094d4d257dbf58811145878db8bbbd62ee1
saraUsama9910 <sarausama80@gmail.com>
2026-01-02 14:17:11 +0200

    items

--- coders-cafe/src/components/services/Services.jsx
+++ coders-cafe/src/components/services/Services.jsx
@@ -74,12 +74,17 @@ dolorem fuga.
 
           {/* card section  */}
 
-<div>
+          <div className='
+grid grid-cols-1
+sm:grid-cols-2
+md:grid-cols-3
+gap-8'>
 
 {servicesData.map((service)=>
 (
-<div>
-    <img src={service.image} />
+<div 
+ >
+    <img src={service.image} alt=''/>
     </div>
 
 ))}
