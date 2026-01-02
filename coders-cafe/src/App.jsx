e19927e7f1db7d38648c89ac46cf9300b876a737
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 01:41:57 +0200

    add app

--- coders-cafe/src/App.jsx
+++ coders-cafe/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/hero/Hero";
 import Services from "./components/services/Services";
 import WhereToBuy from "./components/whereToBuy/WhereToBuy";
+import AppBanner from "./components/appBanner/AppBanner";
 const App = () => {
   return <div >
     <nav className="overflow-x-hidden">
       <Hero/>
 <Services/>
 <WhereToBuy/>
+<AppBanner/>
     </nav>
   </div>;
 };
--- coders-cafe/src/components/appBanner/AppBanner.jsx
+++ coders-cafe/src/components/appBanner/AppBanner.jsx
@@ -5,7 +5,23 @@ import PlayStoreImg from '../../assets/website/play_store.png'
 import{motion} from 'framer-motion'
 const AppBanner = () => {
   return (
-    <div ></div>
+    <div className='container my-14' >
+
+        <div style={BannerStyle}
+        className='sm:min-h-[400px] sm:justify-end
+        sm:items-center 
+        rounded-xl'>
+
+
+<div>
+
+</div>
+
+
+
+
+        </div>
+    </div>
   )
 }
 
