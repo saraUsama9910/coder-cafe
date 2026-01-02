90959d9efeb5803e1fd6ab5bf2309d6993df1f46
saraUsama9910 <sarausama80@gmail.com>
2026-01-02 23:40:19 +0200

    done

--- coders-cafe/src/components/whereToBuy/WhereToBuy.jsx
+++ coders-cafe/src/components/whereToBuy/WhereToBuy.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import worldMap from '../../assets/world-map.png'
 import '../../index.css'
+import{motion} from "framer-motion"
 const WhereToBuy = () => {
   return (
     <div className='container my-36'>
@@ -13,35 +14,61 @@ const WhereToBuy = () => {
 {/* form section*/ }
 <div className='space-y-8'>
  
-<h1 className='
+<motion.h1
+initial={{opacity:0,y:100}}
+whileInView={{opacity:1,y:0}}
+transition={{
+  delay:0.2
+}}
+className='
 text-4xl font-bold
 text-darkGray
 font-serif'>
     Buy our Products From Anywhere
-</h1>
-<div className='flex items-center gap-4'> 
+</motion.h1>
+<motion.div 
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.4
+            }}
+
+className='flex items-center gap-4'> 
    <input type='text' placeholder='Name'
               className='px-4 py-2 border-[1px] border-gray-400/100 rounded-md w-full lg:w-[150px]'/> 
    <input type='email' placeholder='Email'
               className='px-4 py-2 border-[1px] border-gray-400/100 rounded-md w-full ' />
-</div>
-          <div className='flex items-center gap-4'>
+</motion.div>
+          <motion.div 
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.6
+            }}
+          className='flex items-center gap-4'>
             <input type='text' placeholder='Country'
               className='px-4 py-2 border-[1px] border-gray-400/100 rounded-md w-full lg:w-[150px]' />
             <input type='email' placeholder='Zipcode'
-              className='px-4 py-2 border-[1px] border-gray-400/100 rounded-md w-full lg"w-[150px]' />
-          </div>
-          <button className='
-        bg-orange-300/100 
+              className='px-4 py-2 border-[1px] border-gray-400/100 rounded-md w-full lg:w-[150px]' />
+          </motion.div>
+          <motion.button 
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.8
+            }}
+          
+          className='
+        bg-orange-400/100 
         text-white 
         px-4 py-2 
         rounded-md 
-        hover:bg-orange-400/100 
+        hover:bg-orange-300/100 
         duration-200
-      
+        w-full
           '>
             Order Now
-          </button>
+          </motion.button>
             </div>
 {/* world map section */}
             <div className='col-span-2'>
