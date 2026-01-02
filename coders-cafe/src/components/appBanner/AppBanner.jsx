2dfdb6996150860b64bc9bd9aae7177dd708a1f2
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 01:36:44 +0200

    app bannner test

--- coders-cafe/src/components/appBanner/AppBanner.jsx
+++ coders-cafe/src/components/appBanner/AppBanner.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import bannerImg from '../../assets/website/coffee-cover.jpg'
+import AppStoreImg from '../../assets/website/app_store.png'
+import PlayStoreImg from '../../assets/website/play_store.png'
+import{motion} from 'framer-motion'
 const AppBanner = () => {
   return (
-    <div>AppBanner</div>
+    <div ></div>
   )
 }
 
