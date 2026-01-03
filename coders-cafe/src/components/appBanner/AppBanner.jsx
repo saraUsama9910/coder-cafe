bc3cd611636e72df116b903279494cae00d0da44
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 03:17:41 +0200

    done animation on links

--- coders-cafe/src/components/appBanner/AppBanner.jsx
+++ coders-cafe/src/components/appBanner/AppBanner.jsx
@@ -33,22 +33,43 @@ const AppBanner = () => {
 <div>
 <div className='space-y-6
 max-w-xl mx-auto'>
-  <h1 className='
+  <motion.h1
+  initial={{opacity:0,y:100}}
+  whileInView={{opacity:1,y:0}}
+  transition={{
+    type:"spring",
+    stiffness:100,
+    damping:10,
+    delay:0.2,
+  }}
+  
+  className='
     text-2xl 
     font-semibold 
     text-center
     sm:text-4xl
     '>
     Download The App
-    </h1>  
-    <p className='
+    </motion.h1>  
+    <motion.p 
+
+    initial={{ opacity: 0, y: 100 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+    type: "spring",
+    stiffness: 100,
+    damping: 10,
+    delay: 0.3,
+    }}
+
+    className='
     text-center
     sm:px-20
     '>
         Lorem ipsum sit amet consectetur sit
         amet consectetur
         adipisicing elit. Officiis{" "}
-        </p>
+        </motion.p>
         {/*images link */}
         <div className='flex justify-center
         items-center gap-4'>
@@ -57,12 +78,37 @@ max-w-xl mx-auto'>
             className='max-w-[150px] sm:max-w-[120px]
             md:max-w-[200px]'
             >
-            <img src={AppStoreImg} alt=''/>
+            <motion.img
+
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                    delay: 0.4,
+            }}
+
+            
+            src={AppStoreImg} alt=''/>
             </a>
             <a href='#'
             className='max-w-[150px] sm:max-w-[120px]
             md:max-w-[200px]'>
-            <img src={PlayStoreImg} alt='' />
+            <motion.img
+            
+
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 0.4,
+            }}
+
+            
+            src={PlayStoreImg} alt='' />
 
             </a>
         </div>
