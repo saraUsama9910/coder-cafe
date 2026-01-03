dcd6230f249f38caa00eaab196ca338380351c17
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 05:33:23 +0200

    push

--- coders-cafe/src/App.jsx
+++ coders-cafe/src/App.jsx
@@ -4,9 +4,12 @@ import Services from "./components/services/Services";
 import WhereToBuy from "./components/whereToBuy/WhereToBuy";
 import AppBanner from "./components/appBanner/AppBanner";
 import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return <div >
     <nav className="overflow-x-hidden">
+      
+      <Navbar/>
       <Hero/>
 <Services/>
 <WhereToBuy/>
