6e732fd1efa3978e2dfa17653f8df68b408c6a88
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 02:34:10 +0200

    done img

--- coders-cafe/src/components/appBanner/AppBanner.jsx
+++ coders-cafe/src/components/appBanner/AppBanner.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import bannerImg from '../../assets/website/coffee-cover.jpg'
-import AppStoreImg from '../../assets/website/app_store.png'
-import PlayStoreImg from '../../assets/website/play_store.png'
-import{motion} from 'framer-motion'
+import bannerImg from '../../assets/coffee-cover.jpg'
+// import AppStoreImg from '../../assets/website/app_store.png'
+// import PlayStoreImg from '../../assets/website/play_store.png'
+// import{motion} from 'framer-motion'
+
+const BannerStyle={
+    backgroundImage:`url(${bannerImg})`,
+    backgroundSize:'cover',
+    backgroundPosition:'center',
+    backgroundRepeat:'no-repeat',
+    height:'100%',
+    width:'100%',
+};
+
+
 const AppBanner = () => {
   return (
     <div className='container my-14' >
 
         <div style={BannerStyle}
-        className='sm:min-h-[400px] sm:justify-end
+        className='
+        sm:min-h-[400px] 
+        sm:flex
+        sm:justify-end
         sm:items-center 
-        rounded-xl'>
+        rounded-xl'
+        >
 
+      
 
 <div>
 
