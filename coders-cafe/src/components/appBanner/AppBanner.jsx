2ad3f95c1e0b3837054d3adcef29c66476726815
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 03:07:38 +0200

    links

--- coders-cafe/src/components/appBanner/AppBanner.jsx
+++ coders-cafe/src/components/appBanner/AppBanner.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import bannerImg from '../../assets/coffee-cover.jpg'
-// import AppStoreImg from '../../assets/website/app_store.png'
-// import PlayStoreImg from '../../assets/website/play_store.png'
-// import{motion} from 'framer-motion'
+import { FaAppStore } from 'react-icons/fa';
+import AppStoreImg from '../../assets/website/app_store.png'
+import PlayStoreImg from '../../assets/website/play_store.png'
+import{motion} from 'framer-motion'
 
 const BannerStyle={
     backgroundImage:`url(${bannerImg})`,
@@ -30,7 +31,42 @@ const AppBanner = () => {
       
 
 <div>
+<div className='space-y-6
+max-w-xl mx-auto'>
+  <h1 className='
+    text-2xl 
+    font-semibold 
+    text-center
+    sm:text-4xl
+    '>
+    Download The App
+    </h1>  
+    <p className='
+    text-center
+    sm:px-20
+    '>
+        Lorem ipsum sit amet consectetur sit
+        amet consectetur
+        adipisicing elit. Officiis{" "}
+        </p>
+        {/*images link */}
+        <div className='flex justify-center
+        items-center gap-4'>
+            <a
+            href='#'
+            className='max-w-[150px] sm:max-w-[120px]
+            md:max-w-[200px]'
+            >
+            <img src={AppStoreImg} alt=''/>
+            </a>
+            <a href='#'
+            className='max-w-[150px] sm:max-w-[120px]
+            md:max-w-[200px]'>
+            <img src={PlayStoreImg} alt='' />
 
+            </a>
+        </div>
+</div>
 </div>
 
 
