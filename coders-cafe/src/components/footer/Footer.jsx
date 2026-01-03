58beca55a38c5aac6577a3580012896baf7dbc91
saraUsama9910 <sarausama80@gmail.com>
2026-01-03 03:40:03 +0200

    compelete

--- coders-cafe/src/components/footer/Footer.jsx
+++ coders-cafe/src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { 
+    FaFacebook,
+    FaGoogle,
+    FaInstagram,
+    FaPhone,
+    FaTelegram
+} from 'react-icons/fa'
+import {FaMapLocationDot} from 'react-icons/fa6'
+import CreditCards from '../../assets/website/credit-cards.webp'
+import {motion} from 'framer-motion'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-gradient-to-r 
+                    from-orange-300/100 to-orange-500/100
+                        pt-12
+                        pb-8
+                        text-white'>
+            <div className='container'>
+                    <div className='grid grid-cols-1 
+                                    sd:grid-cols-2 
+                                    md:grid-cols-3
+                                    gap-8 '>
+
+                                {/* company details section */}
+                                {/* footer links section */}
+                                {/* social links section */}
+
+            </div>
+        </div>
+    </div>
   )
 }
 
